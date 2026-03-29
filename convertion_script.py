506c7632088dcb234774cbda17e1709aa8c21070
Dima5673 <addictiondima@icloud.com>
2026-03-29 16:40:00 +0300

    Update convertion_script.py

--- convertion_script.py
+++ convertion_script.py
@@ -1,8 +1,11 @@
 import ffmpeg
+import subprocess
 from pathlib import Path
 
 video_formats = {".mov", ".mp4", ".mkv", ".webm", ".avi", ".m4v", ".flv", ".wmv", ".mts", ".m2ts"}
-image_formats = {".jpg", ".jpeg", ".png", ".webp", ".bmp", ".tif", ".tiff", ".heic", "heif"}
-subtitle_formats = {".srt", ".ass", ".ssa", ".vtt", ".webvtt"}
+image_formats = {".jpg", ".jpeg", ".png", ".webp", ".bmp", ".tif", ".tiff", ".heic", ".heif"}
 
-out_video_
+out_video_formats = {".mp4", ".mkv", ".webm", ".mov", ".avi", ".giv"}
+out_image_formats = {".jpg", ".png", ".webp", ".bmp", ".tiff"}
+out_audio_formats = {".mp3", ".m4a", ".aac", ".wav", ".flac", ".opus"}
+
